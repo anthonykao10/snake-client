72073fa283f87f9620b10a436906f8bba2b535d2
anthonykao10 <anthonykao10@gmail.com>
2019-08-28 00:52:45 +0000

    Refactor play.js by pulling input handling logic into separate input.js file

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 // Establish connection with game server
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
 
-// Setup User Interface, to handle user input via stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-
-// Register handleUserInput on stdin
-setupInput().on('data', handleUserInput);
+setupInput();

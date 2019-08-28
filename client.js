19670c26de61a95dd5d9ae37a3fafffc88706ae3
anthonykao10 <anthonykao10@gmail.com>
2019-08-28 02:39:44 +0000

    -Implement sending messages\n-Implement auto movement using setInterval\n-Implement turbo/normal speed\n-Refactor constants into a separate file

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 const connect = function() {
   const conn = net.createConnection({ 
     host: 'localhost',
-    port: 50541
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
@@ -11,12 +12,7 @@ const connect = function() {
   // Send username upon successfully connecting to server
   conn.on('connect', () => {
     console.log('connected to game server...');
-    conn.write('Name: SPX');
-
-    // let clearVal = setInterval(() => {
-    //   conn.write('Move: up');
-    // }, 50);
-    
+    conn.write('Name: AK');
   });
 
   conn.on('data', (data) => {
--- input.js
+++ input.js
@@ -1,3 +1,4 @@
+const { SPEEDNORMALUD, SPEEDNORMALLR, SPEEDTURBOUD, SPEEDTURBOLR } = require('./constants');
 // Stores the active TCP connection object.
 let connection;
 
@@ -12,24 +13,60 @@ const setupInput = function(conn) {
   return stdin;
 }
 
+let clearVal;
+let boost = false;
+let delay; 
+
 const handleUserInput = function(key) {
   switch (key) {
     case 'w':
-      connection.write('Move: up');
-      // console.log('up');
+      delay = boost ? SPEEDTURBOUD : SPEEDNORMALUD;
+
+      clearInterval(clearVal);
+      clearVal = setInterval(() => {
+        connection.write('Move: up');
+      }, delay);
+      // connection.write('Move: up');
       break;
     case 'a':
-      connection.write('Move: left');
-      // console.log('left');
+      delay = boost ? SPEEDTURBOLR : SPEEDNORMALLR;
+
+        clearInterval(clearVal);
+        clearVal = setInterval(() => {
+          connection.write('Move: left');
+        }, delay);
+      // connection.write('Move: left');
       break;
     case 's':
-      connection.write('Move: down');
-      // console.log('down');
+      delay = boost ? SPEEDTURBOUD : SPEEDNORMALUD;
+
+      clearInterval(clearVal);
+      clearVal = setInterval(() => {
+        connection.write('Move: down');
+      }, delay);
+      // connection.write('Move: down');
       break;
     case 'd':
-      connection.write('Move: right');
-      // console.log('right');
+      delay = boost ? SPEEDTURBOLR : SPEEDNORMALLR;
+
+      clearInterval(clearVal);
+      clearVal = setInterval(() => {
+        connection.write('Move: right');
+      }, delay);
+      // connection.write('Move: right');
       break;
+
+    case 'm':
+      connection.write('Say: hello');  
+      break; 
+    case 'n':
+      connection.write('Say: bye');
+      break;   
+
+    case 'p':
+      boost = !boost;
+      break;
+
     case '\u0003':
       process.exit();
   }
